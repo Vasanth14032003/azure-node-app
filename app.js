00b1687c294e4663b2a02dcc1796a4fdbe520da0
vasanthpandian1403 <vasanth.pandian1403@gmail.com>
2026-04-16 01:16:36 +0530

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Azure 🚀");
+  res.send("Hi Hello from Azure 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
